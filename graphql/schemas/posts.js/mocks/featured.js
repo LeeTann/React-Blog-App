63eb380ccf56ac9e245073e17da83d2db2a9432e
ltann <leeroyleeroy18@gmail.com>
2020-06-23 15:14:13 -0700

    Added basic working backend with Koa and GraphQL

---
+++ graphql/schemas/posts.js/mocks/featured.js
@@ -0,0 +1,32 @@
+const moment = require('moment')
+
+module.exports = [
+  {
+    title: 'Can anyone code',
+    date: moment().format('MMMM DD YYYY'),
+    categories: ['Tech Culture', 'Tech News'],
+    link: '#',
+    image: 'trolltunga1tiny.jpg'
+  },
+  {
+    title: 'Using AWS S3 for storing blog images',
+    date: moment().format('MMMM DD YYYY'),
+    categories: ['Cloud'],
+    link: '#',
+    image: 'trolltunga2.jpg'
+  },
+  {
+    title: 'Popular programming Languages in 2020',
+    date: moment().format('MMMM DD YYYY'),
+    categories: ['Tech Culture', 'Tech News'],
+    link: '#',
+    image: 'trolltunga1tiny.jpg'
+  },
+  {
+    title: 'Brain Hacks for Learning to Code',
+    date: moment().format('MMMM DD YYYY'),
+    categories: ['Tech Culture', 'Tech News'],
+    link: '#',
+    image: 'trolltunga1tiny.jpg'
+  },
+]
